ef5b400b90dac170c3113fe93b07d2c921645418
IoanRsX <133961591+IoanRsX@users.noreply.github.com>
2023-05-18 10:48:33 +0100

    Update export group membership for on-prem AD with recursion.ps1
    added more comments

--- export group membership for on-prem AD with recursion.ps1
+++ export group membership for on-prem AD with recursion.ps1
@@ -1,29 +1,31 @@
-﻿# Import the Active Directory module
+# Import the Active Directory module (only if needed, couldn't be bothered to include a check if it's already present)
 Import-Module ActiveDirectory
 
-# Specify the group identity
-$groupIdentity = "Whatever.group@whateverdomain.com"  # Replace with the actual group name or identity
+# Specify the group identity (this is hardcoded, too lazy to include a prompt)
+$groupIdentity = "whatever.group@whateverdomain.com"  # Replace with the actual gorup name or identity (can also be ObjectID if you wanna get fancy)
 
-# Retrieve group members recursively
+# Retrieve group members recursively and we look for properties of each AD user (in this case Given Name, Surname, Email - replace with any other properties from Get-ADUser)
 $groupMembers = Get-ADGroupMember -Identity $groupIdentity -Recursive |
                 Where-Object { $_.objectClass -eq 'user' } |
                 Get-ADUser -Properties GivenName, Surname, EmailAddress
 
-# Create an empty array to store user details
+# Create an empty array to store user details 
 $userDetails = @()
 
-# Iterate through each user and extract required details
+# Iterate through each user and extract required details and structure the upcominfg .csv export from that array
 foreach ($member in $groupMembers) {
     $user = @{
         "First Name"   = $member.GivenName
         "Last Name"    = $member.Surname
         "Email Address" = $member.EmailAddress
+
+# Create a new PSObject to store our structured data for each user
     }
     $userDetails += New-Object PSObject -Property $user
 }
 
-# Export the user details to a CSV file
-$csvFilePath = "c:\whatever\path.csv"  # Specify the path and filename for the CSV file
+# Export the user details to a CSV file - change the file path to whatever floats your boat 
+$csvFilePath = "c:\whatever\GroupUserDetails.csv"  
 $userDetails | Export-Csv -Path $csvFilePath -NoTypeInformation
 
 # Display a confirmation message
